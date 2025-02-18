0e025640027570b32dd4e6e15b7d98695fdea989
Devansh Sharma <msddevansh@gmail.com>
2025-02-18 12:37:53 +0000

    variable & datatypes

---
+++ js_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert("hello") --> Error cause we are using node js not browser....!
+
+let name="devansh"
+let rollno=12
+let isLoggedIn=false
+
+/*  number=> 2 ki power 53
+bigint 
+string=> " "
+boolean=> true/false
+NULL=> stand alone value{represents empty value}
+undefined=>
+    symbole=> unique
+
+*/
+
+console.log(typeof null)
---
+++ js_basics/variables.js
@@ -0,0 +1,12 @@
+const accountID=1234
+let accountEmail="abcd@gmail.com"
+var accountPassword="12345678"
+accountCity="Jaipur"
+let accountSalary
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountID,accountEmail,accountSalary,accountPassword,accountCity])
