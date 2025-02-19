787aff3a961dd9dbe757c7cb12d2e1076f7aae05
Devansh Sharma <msddevansh@gmail.com>
2025-02-19 08:52:28 +0000

    conversion

---
+++ js_basics/conversion.js
@@ -0,0 +1,54 @@
+let score="33"
+// console.log(score)
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+score=33
+
+// console.log(typeof score)
+// valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// score=null
+// console.log(typeof score)
+
+// valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+score=undefined
+// console.log(typeof score)
+
+//  valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+conversion-->
+
+1) to Number-
+"33"=>33
+true=>1
+false=>0
+"33abc"=>NaN{not an number}
+
+2) to boolean 
+1=>true
+0=> false
+" "=>false
+
+*/
+
+let someNumber = 33
+let inString= String(someNumber)
+
+console.log(inString)
+console.log(typeof(inString))
