969839aa268f050d6a87e84ef414a2f410d19ffe
Devansh Sharma <msddevansh@gmail.com>
2025-04-12 13:50:43 +0000

    math & num

---
+++ js_basics/num_and_maths.js
@@ -0,0 +1,39 @@
+/*
+const score = 400
+console.log(score);
+console.log(typeof(score));
+
+const balance = new  Number(500)
+console.log(balance);
+console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+
+const otherNum = 123.8596
+console.log(otherNum.toPrecision(4)); // 123.9
+console.log(otherNum.toPrecision(3)); // 124
+
+const num = new Number(1000000)
+console.log(num.toLocaleString('en-In'));
+*/
+
+//================================= Maths ++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4)); //4
+// console.log(Math.ceil(4.4)); // 5
+// console.log(Math.floor(4.4)); // 4
+
+// console.log('Minimum : ', Math.min(4,7,8,3)," Maximum : ",Math.max(4,7,8,3));
+
+// console.log(Math.random()); // always have value in between 0 to 1
+// console.log((Math.random()*10) + 1); // always intialize with 1 and and with 10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*11) + 10); // always intialize with 1 and and with 10
+
+// console.log(Math.floor(Math.random()*(max-min +1)) + min);
+
+
